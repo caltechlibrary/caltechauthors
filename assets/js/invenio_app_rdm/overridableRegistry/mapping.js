ee3b6cb05692d38bdd899cd85f3b6b4e4fc82465
Tom Morrell <tmorrell@caltech.edu>
2023-07-27 23:19:41 +0000

    Customize publisher help text

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -4,8 +4,40 @@
 // Invenio App RDM is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-/**
- * Add here all the overridden components of your app.
- */
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-export const overriddenComponents = {}
+import { FieldLabel, TextField } from "react-invenio-forms";
+import { i18next } from "@translations/invenio_rdm_records/i18next";
+
+export class PublisherField extends Component {
+  render() {
+    const { fieldPath, label, labelIcon, placeholder } = this.props;
+
+    return (
+      <TextField
+        fieldPath={fieldPath}
+        helpText={i18next.t(
+          "If you're submitting a published journal article, ignore this field and use the 'Journal name' field in the 'Caltech Custom Metadata' section. If you're creating a DOI, this field is important as it shows up in the citation."
+        )}
+        label={<FieldLabel htmlFor={fieldPath} icon={labelIcon} label={label} />}
+        placeholder={placeholder}
+      />
+    );
+  }
+}
+
+PublisherField.propTypes = {
+  fieldPath: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  labelIcon: PropTypes.string,
+  placeholder: PropTypes.string,
+};
+
+PublisherField.defaultProps = {
+  label: i18next.t("Publisher"),
+  labelIcon: "building outline",
+  placeholder: i18next.t("Publisher"),
+};
+
+export const overriddenComponents = {"InvenioAppRdm.Deposit.PublisherField.container":PublisherField,}; 
