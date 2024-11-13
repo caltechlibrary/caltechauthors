2c32c1aa09bfbe8c49ef344a4e652e46e2715f9a
Tom Morrell <tmorrell@caltech.edu>
2024-11-12 16:43:12 -0800

    Initial setup scriopt

--- scripts/setup_test_instance.py
+++ scripts/setup_test_instance.py
@@ -1,4 +1,34 @@
 import os
+import requests
 from caltechdata_api import get_metadata
 
+token = os.environ["RDMTOK"]
+host = "https://authors.caltechlibrary.dev"
+
+# Create a caltechauthors community
+
+community = {
+    "access": {
+        "visibility": "public",
+        "member_policy": "open",
+        "record_policy": "closed",
+        "owned_by": [{'"user":2'}],
+    },
+    "slug": "caltechauthors",
+    "metadata": {"title": "CaltechAUTHORS", "description": "Caltech Authors"},
+}
+
+response = requests.post(
+    host + "/api/communities/",
+    json=community,
+    headers={"Authorization": f"Bearer {token}"},
+)
+if response.status_code != 201:
+    print("Failed to create community")
+    print(response.text)
+    exit()
+
+community_id = response.json()["id"]
+
+print("Created community with id", community_id)
 
