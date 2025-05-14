7a9f0c8b12f462e1541d251468018ac80d157688
Tom Morrell <tmorrell@caltech.edu>
2025-05-14 17:18:04 +0000

    Script for deleting datacite pid

---
+++ scripts/delete_datacite_pid.py
@@ -0,0 +1,24 @@
+import click, os
+
+from flask.cli import with_appcontext
+from invenio_db import db
+from invenio_files_rest.models import ObjectVersion
+from invenio_utilities_tuw.utils import get_identity_for_user, get_record_service
+
+@click.command('delete_pid')
+@click.argument('pid', type=str)
+@with_appcontext
+def delete_pid(pid):
+    """Delete pid"""
+    service = get_record_service()
+    provider = service.pids.pid_manager._get_provider("doi", "datacite")
+    pid = provider.get(pid_value=pid)
+    print(pid)
+    pid.delete()
+    print(pid)
+    print('Deleted')
+    db.session.delete(pid)
+    db.session.commit()
+
+if __name__=="__main__":
+    delete_pid()
